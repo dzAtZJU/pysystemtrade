1e5e9bf2262140cc0827f5bcf6ca6afa611dd382
you@example.com <you@example.com>
2022-11-13 16:49:37 +0800

    draft

---
+++ paper/systems/simplesystem.py
@@ -0,0 +1,39 @@
+from sysdata.sim.csv_futures_sim_data import csvFuturesSimData
+from sysdata.config.configdata import Config
+
+from systems.forecasting import Rules
+from systems.basesystem import System
+from systems.forecast_combine import ForecastCombine
+from systems.forecast_scale_cap import ForecastScaleCap
+from systems.positionsizing import PositionSizing
+from systems.portfolio import Portfolios
+from systems.accounts.accounts_stage import Account
+from systems.rawdata import RawData
+
+
+def simplesystem(data=None, config=None, log_level="on"):
+    """
+    Example of how to 'wrap' a complete system
+    """
+    if config is None:
+        config = Config("paper.systems.simplesystemconfig.yaml")
+    if data is None:
+        data = csvFuturesSimData()
+
+    my_system = System(
+        [
+            Account(),
+            Portfolios(),
+            PositionSizing(),
+            ForecastCombine(),
+            ForecastScaleCap(),
+            Rules(),
+            RawData(),
+        ],
+        data,
+        config,
+    )
+
+    my_system.set_logging_level(log_level)
+
+    return my_system
