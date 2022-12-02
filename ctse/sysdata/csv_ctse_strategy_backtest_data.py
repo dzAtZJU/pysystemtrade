c38d9931226ffbdaa8220441407144a22c47ecae
you@example.com <you@example.com>
2022-12-02 17:20:25 +0800

    draft

--- ctse/sysdata/csv_ctse_strategy_backtest_data.py
+++ ctse/sysdata/csv_ctse_strategy_backtest_data.py
@@ -5,7 +5,7 @@ from syscore.objects import arg_not_supplied
 from syslogdiag.log_to_screen import logtoscreen
 from ctse.sysobjects.ctse_strategy_backtest import ctseStrategyBacktest
 
-CTSE_STRATEGY_BACKTEST_DIRECTORY = "ctse.data.strategy_backtest"
+CTSE_STRATEGY_BACKTEST_DIRECTORY = "ctse.data.backtest"
 DATE_INDEX_NAME = 'tr_datetime'
 
 class csvCTSEStrategyBacktestData:
@@ -28,8 +28,14 @@ class csvCTSEStrategyBacktestData:
     def datapath(self):
         return self._datapath
 
-    def read(self, instrument: str, strategy:str) -> ctseStrategyBacktest:
-        filename = self._filename_given(instrument, strategy)
+    def read(self, timeframe, instrument: str, strategy:str,
+        cols=[
+            ctseStrategyBacktest.OPEN,
+            ctseStrategyBacktest.CLOSE,
+            ctseStrategyBacktest.OPERATE_FLAG,
+            ctseStrategyBacktest.NAV
+            ]) -> ctseStrategyBacktest:
+        filename = self._filename_given(timeframe, instrument, strategy)
 
         try:
             tmp = pd_readcsv(filename, date_index_name=DATE_INDEX_NAME)
@@ -40,8 +46,12 @@ class csvCTSEStrategyBacktestData:
             )
             return None
 
-        return ctseStrategyBacktest(tmp)
+        return ctseStrategyBacktest(tmp)[cols]
 
-    def _filename_given(self, instrument_code: str, strategy:str):
-        filename = get_pathname_for_package_from_list([self.datapath, strategy, "%s.csv" % (instrument_code)])
+    def _filename_given(self, timeframe:str, instrument_code: str, strategy:str):
+        filename = get_pathname_for_package_from_list([self.datapath, timeframe, strategy, "{}.csv".format(instrument_code)])
         return filename
+
+if __name__ == '__main__':
+    r = csvCTSEStrategyBacktestData().read('1_B', 'RM', 'dma_long')
+    print(r)
